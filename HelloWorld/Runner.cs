0fdf7f9c5a9459ae766bd7cb17950fb819d31777
Chris Hudson <chris@hudsondarling.net>
2018-11-06 12:14:48 +1100

    Create runner class
    Create run method so that we areworking in a non-static method

---
+++ HelloWorld/Runner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class Runner
+    {
+        public Runner()
+        {
+
+        }
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is line 2");
+            Console.WriteLine("This is line 3");
+        }
+    }    
+}
